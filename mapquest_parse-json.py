82f382724e8324d1580a5d13271c4e99b555130d
Leewenctt <leewenctt@gmail.com>
2024-12-21 15:46:58 +0800

    Feature added: Allows the user to select traversal method (Car, Bike, Foot)

--- mapquest_parse-json.py
+++ mapquest_parse-json.py
@@ -14,36 +14,39 @@ while True:
     if dest.lower() in ["quit", "q"]:
         break
 
-    url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
-    print("URL: " + url)
+    mode = input("Choose vehicle type (Car, Bike, Foot): ").lower()
+    mode_selection = {"car": "fastest", "bike": "bicycle", "foot": "pedestrian"}.get(mode, "fastest")
+
+    url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest, "routeType": mode_selection})
 
     json_data = requests.get(url).json()
     json_status = json_data["info"]["statuscode"]
 
     if json_status == 0:
-        print("API Status: " + str(json_status) + " = A successful route call.\n")
-        print("=============================================")
-        print("Directions from " + orig + " to " + dest)
-        print("Trip Duration: " + json_data["route"]["formattedTime"])
+        print("\nAPI Status: " + str(json_status) + " = A successful route call.")
+        print("URL: " + url)
+        print("\n===============================================================")
+        print("Directions from " + orig + " to " + dest + " by " + mode + ":")
         distance_km = json_data["route"]["distance"] * 1.61
-        print("Kilometers: " + str("{:.2f}".format(distance_km)))
+        print("  • Distance: " + str("{:.2f}".format(json_data["route"]["distance"])) + "mi / " + str("{:.2f}".format(distance_km)) + "km")
+        print("  • Estimated Trip Duration: " + json_data["route"]["formattedTime"])
         
-        fuel_used = distance_km / fuel_efficiency
-        print("Fuel Used (Ltr): " + str("{:.2f}".format(fuel_used)))
+        if mode_selection == "fastest":
+            fuel_used = distance_km / fuel_efficiency
+            print("  • Estimated Fuel Usage (L): " + str("{:.2f}".format(fuel_used)))
         
-        print("=============================================")
-
+        print("\nTurn-by-turn Directions:")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
-            print(each["narrative"] + " (" + str("{:.2f}".format(each["distance"] * 1.61)) + " km)")
-        print("=============================================\n")
+            print("  ➢  " + each["narrative"] + " (" + str("{:.2f}".format(each["distance"] * 1.61)) + " km)")
+        print("===============================================================\n")
 
-    elif json_status == 402:
-        print("**********************************************")
+    elif json_status == 402: 
+        print("*************************************************************")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
         print("**********************************************\n")
 
     elif json_status == 611:
-        print("**********************************************")
+        print("*************************************************************")
         print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
         print("**********************************************\n")
 
